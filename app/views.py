7b4685f44be80b6df8178385879040c1ca65ddd4
Aviraj Khare <thisisavirajkhare@gmail.com>
2022-12-02 21:35:47 +0530

    [Aviraj] Initial commit

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def alert(request):
+    if request.method == 'GET':
+        pass
+    if request.method == 'POST':
+        pass
+
+def login(request):
+    if request.method == 'GET':
+        pass
+    if request.method == 'POST':
+        pass
+
+def dashboard(request):
+    if request.method == 'GET':
+        pass
