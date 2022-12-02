6e4cc39ab00e7c1de56aeffe78a8d6cbb7a32fc8
Aviraj Khare <thisisavirajkhare@gmail.com>
2022-12-03 00:58:38 +0530

    [Aviraj] Added csrf_exempt to views

--- app/views.py
+++ app/views.py
@@ -1,18 +1,22 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 
 # Create your views here.
+@csrf_exempt
 def alert(request):
     if request.method == 'GET':
         pass
     if request.method == 'POST':
         pass
 
+@csrf_exempt
 def login(request):
     if request.method == 'GET':
         pass
     if request.method == 'POST':
         pass
 
+@csrf_exempt
 def dashboard(request):
     if request.method == 'GET':
         pass
